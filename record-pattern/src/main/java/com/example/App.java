290a4ceee0d1b28e461ff5058e356eacdc99a020
SitoAA <ahw.alonso@gmail.com>
2026-04-22 13:35:42 +0200

    sin record patter

--- record-pattern/src/main/java/com/example/App.java
+++ record-pattern/src/main/java/com/example/App.java
@@ -1,10 +1,25 @@
 package com.example;
 
-/**
- * Hello world!
- */
+
 public class App {
+	static void imprimeDetalles(Object obj) {
+		if (obj instanceof book book) {
+			System.out.println("Titulo del libro: "+ book.title());
+			System.out.println("Autor del libro" + book.author());
+		}
+	}
+	
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	
+        // CREAR UN OBJETO TIPO RECORD BOOK
+    	
+    	// book libro = new book("java es bueno", "yo mismo ninio");
+    	
+    	book libro = book.builder()
+    			.title("Java es bueno")
+    			.author (" yo mismo")
+    			.build();
+    	
+    	imprimeDetalles(libro);
     }
 }
---
+++ record-pattern/src/main/java/com/example/book.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import lombok.Builder;
+
+@Builder
+
+
+public record book(String title, String author) {
+
+}
